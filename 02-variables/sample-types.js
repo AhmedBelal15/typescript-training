45a393fb3c7f34d6a196e83bb74c3ee45dcbe5be
ahmedbelal15 <ahmbelal95@gmail.com>
2021-04-02 00:57:07 +0200

    variables

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,7 @@
+var found = true;
+var grade = 88.6;
+var firstName = 'Ahmed';
+var lastName = 'Belal';
+console.log(found);
+console.log('The grade is ' + grade);
+console.log("My name is " + firstName + " " + lastName);
---
+++ 02-variables/sample-types.ts
@@ -0,0 +1,18 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = 'Ahmed';
+let lastName: string = 'Belal';
+
+// Let's break it
+// we should use noEmitOnErrors
+/*
+found = 0;
+grade = 'A';
+firstName = 15
+*/
+
+console.log(found);
+console.log('The grade is '+ grade);
+console.log(`My name is ${firstName} ${lastName}`);
+
+
