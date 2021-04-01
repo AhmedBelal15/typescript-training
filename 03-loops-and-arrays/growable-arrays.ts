cf10221fc31a9a868ff3e37d4f302b6108be7406
ahmedbelal15 <ahmbelal95@gmail.com>
2021-04-02 01:56:43 +0200

    Loops and Arrays

---
+++ 03-loops-and-arrays/growable-arrays.ts
@@ -0,0 +1,9 @@
+let sportsTwo: string[] = ['Golf', 'Cricket', 'Tennis'];
+
+sportsTwo.push('Baseball');
+sportsTwo.push('Football');
+
+for(let sport of sportsTwo){
+    console.log(sport);
+    
+}
---
+++ 03-loops-and-arrays/sports.ts
@@ -0,0 +1,9 @@
+let sportsOne: string[] = ['Golf', 'Cricket', 'Tennis', 'Swimming'];
+
+for(let sport of sportsOne){
+    if(sport === 'Tennis'){
+        console.log(`My favorite sport is ${sport}`);
+    } else {
+        console.log(sport); 
+    }
+}
