fede3440dace746a9f2832c538e206ada0fad681
ahmedbelal15 <ahmbelal95@gmail.com>
2021-04-03 01:54:28 +0200

    abstract classes

---
+++ 06-abstract-class/Driver.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Rectangle_1 = require("./Rectangle");
+var myRectangle = new Rectangle_1.Rectangle(5, 10, 5, 10);
+console.log(myRectangle.calculateArea());
---
+++ 06-abstract-class/Driver.ts
@@ -0,0 +1,5 @@
+import { Rectangle } from './Rectangle'
+
+const myRectangle = new Rectangle(5, 10, 5, 10);
+
+console.log(myRectangle.calculateArea());
